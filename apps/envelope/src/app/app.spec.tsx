f00ed6b956999cef5316a7536bd641de26704e18
Novi Dwi Cahya <novidwicahya19@gmail.com>
2023-01-22 15:43:30 +0700

    Fixing testing on app.spec.tsx

--- apps/envelope/src/app/app.spec.tsx
+++ apps/envelope/src/app/app.spec.tsx
@@ -5,21 +5,13 @@ import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { baseElement } = render(<App />);
 
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a href to login page', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { getByText } = render(<App />);
 
     const href = getByText(/Login Page/i).getAttribute('href');
     expect(href).toBe('/login');
